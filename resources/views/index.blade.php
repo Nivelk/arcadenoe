6381800f1b1a6a761e8e28f316f6fef10c7bb499
42143 <aslan-k@hotmail.com>
2017-08-08 02:54:56 -0300

    atualização do site clinica veterinaria arca de noe Itaim paulista

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,175 +1,7 @@
-<!DOCTYPE html>
-<html lang="br">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="Content-Language" content="pt-br"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!--google-->
-		<link rel="canonical" href="http://www.arcadenoevet.com.br/"/>
-		<link rel="alternate" hreflang="pt-br" href="http://www.arcadenoevet.com.br/"/>
-	<!--SEO-->
-		<meta name="author" content="Nivelk Agência digital"/>
-		<meta name="classification" content="Clínica veterinária"/>
-		<meta name="title" content="Clinica Veterinária Itaim Paulista | Arca de Noé"/>
-		<meta name="description" content="Agora, sob nova administração, a clínica veterinária Arca de Noé, muito conhecida e localizada na região do Itaim Paulista em São Paulo, encontra-se em pleno funcionamento, proporcionando aos seus clientes um serviço de alta qualidade."/>
-		<meta name="URL" content="http://www.arcadenoevet.com.br/"/>
-		<meta name="language" content="pt-br"/>
-	<!--Rede social: Facebook / open graph-->
-		<meta property="og:locale" content="pt_BR"/>
-        <meta property="og:url" content="http://www.arcadenoevet.com.br/"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Clinica Veterinária Itaim Paulista | Arca de Noé"/>
-        <meta property="og:image" content=""/>
-        <meta property="og:description" content="Agora, sob nova administração, a clínica veterinária Arca de Noé, muito conhecida e localizada na região do Itaim Paulista em São Paulo, encontra-se em pleno funcionamento, proporcionando aos seus clientes um serviço de alta qualidade."/>
-        <meta property="og:site_name" content="Clinica Veterinária Arca de Noé Itaim Paulista "/>
-        <meta property="article:author" content="https://www.facebook.com/nivelk.com.br/"/>
-        <meta property="article:publisher" content="https://www.facebook.com/arcadenoevetitaimpaulista/"/>
-        <meta property="fb:admins" content=""/>
-	<!--ferramentas google--> 
-    <!-- Bootstrap core CSS -->
-        <link href="/js/componentes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom fonts for this template -->
-        <link href="/css/componentes/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <!-- Plugin CSS -->
-        <link href="/js/componentes/magnific-popup/magnific-popup.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-        <link href="/css/creative.css" rel="stylesheet">
-        <title>Clinica Veterinária Itaim Paulista | Arca de Noé</title>
-    <!-- Temporary navbar container fix -->
-        <style>
-        .navbar-toggler {
-            z-index: 1;
-        }
-
-        @media (max-width: 576px) {
-            nav > .container {
-                width: 100%;
-            }
-        }
-        </style>
-    </head>
-
-<body id="page-top">
-
-    <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-toggleable-md navbar-light" id="mainNav">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="container">
-            <a class="navbar-brand" href="{{route('home')}}">
-                <svg>
-                    <text transform="matrix(1 0 0 1 85.1373 56.18)" class="st0 st1">CLÍNICA VETERINÁRIA</text>
-                    <g>
-                        <path class="st0 st1" d="M2,35.2c0,0,0.1,0,0.1,0c5.4-4,11.5-6.8,17.4-9.8c4-2,8.1-3.9,12.3-5.4c0.1,0,0.2-0.1,0.4-0.1
-                            c2.1-0.9,4.2-0.8,6.4,0c5.4,2,10.6,4.4,15.7,6.9c4.3,2.2,8.6,4.3,12.6,7c0.5,0.3,0.9,0.7,1.4,1c0.5,0.3,0.6,0.7,0.4,1.2
-                            c-1,2.8-1.8,5.6-2.9,8.4c-1.7,4.3-3.5,8.6-5.3,12.8c-0.1,0.2-0.2,0.4-0.4,0.8c-0.3-0.4-0.6-0.7-0.8-1c-2.8-4.2-4.8-8.7-6.3-13.4
-                            c-0.9-2.7-2.3-5-4.3-7.1c-2.5-2.6-4.8-5.5-7.1-8.3c-0.4-0.5-0.7-1-1.6-0.9c-0.1,0.6-0.1,1.2-0.2,1.9c-1-0.5-1.1-2-2.6-2.1
-                            c0.2,1,0.2,1.8,0.5,2.7c0.3,1.1,0.8,2.1,1.2,3.2c0.5,1.4,0.3,1.9-1,2.7c-0.8,0.5-1.6,1.1-2.2,1.7c-0.5,0.5-1,1.2-1.3,1.9
-                            c-0.3,0.7-0.7,1.3-1.4,1.7c-1.6,0.9-3.1,1.8-4.7,2.8c-0.6,0.4-1.1,0.8-1.6,1.2c-0.5,0.4-0.6,0.8-0.2,1.4c0.3,0.4,0.7,0.8,1.2,1.1
-                            c2,1.2,4.1,1.6,6.4,1.4c0.6-0.1,1.2-0.1,1.8-0.1c2.3,0.1,3.5,1.1,3.8,3.3c0.4,3.2,0.3,6.4-0.5,9.5c-0.2,0.6-0.4,1.3-0.8,1.8
-                            c-0.9,1.2-1.1,2.5-1,3.9c0,0.9,0.1,1.7,0.2,2.6c-2.9,0-5.8,0-8.7,0c-1.2-0.4-1.1-0.4-1.2-1.7c-0.1-0.8-0.3-1.6-0.5-2.4
-                            c-0.1-0.4-0.2-0.7-0.4-1.3c1.8,0.7,2.8-0.5,4-1.4c0,0,0,0,0.1,0c0.8-0.6,0.9-1,0.5-2c-0.3-0.6-0.5-1.3-0.6-2
-                            c-0.2-1.4-0.3-2.8-1.5-3.7c-0.7-0.5-0.6-1.1-0.4-1.7c0.2-0.4,0.5-0.7,0.7-1.1c0.2-0.3,0.4-0.7,0.6-1.1c-1.4-0.3-1.9,0.9-2.9,1.3
-                            c0-0.3,0.1-0.5,0.1-0.6c0-0.4,0-1-0.2-1.2c-0.3-0.2-0.9-0.1-1.3,0c-0.7,0.3-1.3,0.8-2,1.2c-0.6,0.4-1.3,0.8-2,0.9
-                            c-1.3,0.3-2.3,0.9-3.1,2c-0.9,1.2-1.4,2.5-1.8,3.9c-0.4,1.5-0.8,3.1-1.3,4.6c-0.1,0.5-0.4,0.9-0.6,1.3c-0.1,0.1-0.2,0.1-0.3,0.2
-                            c-0.3-0.4-0.7-0.8-0.9-1.3c-0.7-1-1.4-2-1.9-3.1c-3.1-6-5.6-12.3-7.8-18.7c-0.7-2.1-1.4-4.2-2.1-6.2C2,35.5,2,35.4,2,35.2z"/>
-                        <path class="st0 st1" d="M16.9,15.8c0.8-0.5,1.4-0.9,2.1-1.2c5.2-3,10.4-5.9,15.7-8.9c0.6-0.3,1-0.4,1.6,0C41.8,8.8,47.4,12,53,15.2
-                            c0.3,0.2,0.6,0.4,1,0.6c-0.1,0.3-0.3,0.6-0.4,0.9c-0.9,2-1.7,4.1-2.6,6.1c-0.2,0.4-0.4,0.5-0.8,0.3c-2.1-0.9-4.2-1.9-6.3-2.8
-                            c-1.7-0.7-3.4-1.4-5.2-2c-2.3-0.9-4.5-0.8-6.8,0.1c-3.4,1.4-6.9,2.8-10.3,4.2c-0.5,0.2-1.1,0.4-1.6,0.6
-                            C19,20.8,17.9,18.3,16.9,15.8z"/>
-                        <path class="st0 st1" d="M10,17.9c-0.2-0.3-0.3-0.6-0.4-0.8C9,15.9,9,15.9,10.1,15.3c4.1-2.3,8.1-4.6,12.2-6.9c4-2.3,8-4.5,12-6.8
-                            c0.8-0.4,1.4-0.4,2.2,0c7.9,4.5,15.9,9,23.8,13.5c0.5,0.3,0.9,0.6,1.5,0.9c-0.4,0.6-0.7,1.2-1,1.8c-0.4-0.2-0.8-0.4-1.1-0.5
-                            C53,13.6,46.4,9.8,39.8,6.1c-1.2-0.7-2.4-1.3-3.6-2c-0.6-0.3-1-0.3-1.5,0c-4,2.3-8,4.5-12,6.8c-3.7,2.1-7.4,4.2-11.1,6.3
-                            C11,17.4,10.5,17.6,10,17.9z"/>
-                    </g>
-                    <g>
-                        <path class="st0 st1" d="M191,36.9c0-3.8,0-7.6,0-11.5c1.4,0,2.9,0,4.4,0c0,5.7,0,11.4,0,17.2c-1.4,0-2.9,0-4.3,0
-                            c-0.1,0-0.3-0.2-0.4-0.3c-2.1-2.9-4.2-5.7-6.3-8.6c-0.6-0.8-1-1.8-1.4-2.6c0,3.8,0,7.6,0,11.5c-1.4,0-2.8,0-4.3,0
-                            c0-5.7,0-11.4,0-17.2c1.4,0,2.9,0,4.3,0c0.1,0,0.3,0.2,0.4,0.3c2.1,2.8,4.2,5.7,6.2,8.5C190.1,35,190.5,36,191,36.9z"/>
-                        <path class="st0 st1" d="M92.9,42.6c-1.4,0-2.8,0-4.2,0c-0.2,0-0.4-0.2-0.5-0.4c-0.2-0.7-0.5-1.5-0.6-2.2c-0.1-0.4-0.2-0.5-0.6-0.5
-                            c-1.9,0-3.8,0-5.7,0c-0.4,0-0.5,0.2-0.6,0.5c-0.2,0.9-0.5,1.7-0.7,2.6c-1.5,0-3.1,0-4.6,0c0.1-0.2,0.1-0.4,0.2-0.6
-                            c1.8-5.3,3.7-10.7,5.5-16c0.2-0.5,0.4-0.7,0.9-0.6c1.6,0,3.2,0,4.7,0c0.2,0,0.6,0.2,0.6,0.4c1.9,5.5,3.8,11.1,5.7,16.6
-                            C92.9,42.4,92.9,42.5,92.9,42.6z M84.1,28.3c-0.8,2.6-1.5,5.2-2.3,7.8c1.6,0,3.1,0,4.7,0"/>
-                        <path class="st0 st1" d="M148.8,32.2c0-2.5,0-5.2,0-7.9c1.3,0,2.6,0,3.9,0c0,6.1,0,12.1,0,18.2c-1.2,0-2.5,0-3.8,0c0-0.6,0-1.1,0-1.8
-                            c-0.2,0.3-0.4,0.4-0.5,0.5c-2.1,2.8-6.5,2.2-8-0.5c-1.5-2.7-1.5-5.4,0-8.1c1.2-2.1,3.9-3,6.2-2C147.4,31,148.1,31.7,148.8,32.2z
-                             M149,36.7c0-1.3-0.5-2.3-1.4-2.7c-1.3-0.7-3.1-0.2-3.7,1c-0.8,1.5-0.3,3.7,1,4.4c0.2,0.1,0.4,0.2,0.7,0.2
-                            C147.6,40,149,38.8,149,36.7z"/>
-                        <path class="st0 st1" d="M125.3,32.4c0-0.5,0-1.1,0-1.7c1.3,0,2.5,0,3.8,0c0,4,0,7.9,0,11.9c-1.2,0-2.5,0-3.8,0c0-0.5,0-1.1,0-1.7
-                            c0,0-0.1,0-0.1-0.1c-0.1,0.2-0.3,0.3-0.4,0.5c-2.3,2.7-6.5,2.4-8.1-0.7c-1.3-2.4-1.3-5-0.1-7.5c1.6-3.4,6.8-3.7,8.4-0.7
-                            c0,0.1,0.1,0.2,0.1,0.2C125.2,32.5,125.2,32.5,125.3,32.4z M125.4,36.7c0-1.3-0.4-2.2-1.3-2.7c-1.4-0.8-3.3-0.3-3.9,1.1
-                            c-0.8,1.7-0.2,3.8,1.3,4.4c0.2,0.1,0.4,0.1,0.6,0.1C124.2,39.9,125.4,38.8,125.4,36.7z"/>
-                        <path class="st0 st1" d="M204.8,43.1c-3.8,0-6.6-2.9-6.5-6.6c0-3.5,3-6.3,6.7-6.2c3.7,0,6.6,2.9,6.6,6.4
-                            C211.4,40.3,208.4,43.1,204.8,43.1z M207.3,36.8c0-1.5-0.6-2.5-1.7-2.8c-1.4-0.4-2.7,0.2-3.1,1.5c-0.6,1.7,0.1,3.5,1.4,4
-                            c0.1,0,0.2,0.1,0.3,0.1C206,39.9,207.3,38.8,207.3,36.8z"/>
-                        <path class="st0 st1" d="M159.2,37.5c-0.1,1.3,0.5,2.3,1.5,2.7c1.4,0.5,2.8,0.1,3.8-1.2c1,0.4,2,0.8,3.1,1.2c-0.4,1-1.1,1.7-1.9,2.1
-                            c-2.6,1.2-5.3,1.2-7.8-0.3c-2.2-1.4-3.2-4.1-2.5-6.8c0.6-2.5,2.8-4.5,5.4-4.7c2.4-0.2,4.5,0.2,6.1,2.2c1.1,1.4,1.4,3.1,1.4,4.9
-                            C165.2,37.5,162.2,37.5,159.2,37.5z M164,35.1c0-1.3-1-2.2-2.2-2.2c-1.4,0-2.4,0.8-2.5,2.2C160.9,35.1,162.5,35.1,164,35.1z"/>
-                        <path class="st0 st1" d="M226.3,37.5c-3.1,0-6.1,0-9.1,0c-0.1,1.3,0.5,2.3,1.5,2.7c1.3,0.5,2.7,0.1,3.8-1.2c0.5,0.2,1,0.4,1.5,0.6
-                            c0.5,0.2,1,0.4,1.6,0.6c-0.3,0.8-0.8,1.4-1.5,1.8c-2.4,1.6-6.7,1.3-8.8-0.6c-2.1-1.9-2.5-4.3-1.7-6.8c0.8-2.5,2.6-4,5.2-4.3
-                            c1-0.1,2.1-0.1,3.1,0.2c2.6,0.7,3.8,2.6,4.2,5.1C226.3,36.3,226.3,36.8,226.3,37.5z M222.1,35.1c-0.1-1.3-1-2.2-2.2-2.3
-                            c-1.4,0-2.3,0.8-2.5,2.3C219,35.1,220.5,35.1,222.1,35.1z"/>
-                        <path class="st0 st1" d="M113.7,38.8c0,1.1,0,2.2,0,3.2c0,0.2-0.2,0.4-0.4,0.4c-3.2,1.5-8.2,0.3-8.7-4.9c-0.2-2.3,0.3-4.4,2.1-5.9
-                            c2-1.6,4.1-1.7,6.5-0.9c0.4,0.1,0.6,0.4,0.6,0.8c0,0.9,0,1.9,0,2.8c-2.3-1.1-3.5-1.1-4.4,0.1c-0.9,1.1-0.9,3.2,0,4.2
-                            C110.3,39.9,111.6,39.9,113.7,38.8z"/>
-                        <path class="st0 st1" d="M103.6,30.5c-0.1,1.3-0.2,2.5-0.4,3.7c-0.3,0-0.5-0.1-0.8-0.1c-2.1-0.4-3.6,0.7-3.8,2.8
-                            c-0.1,1.6-0.1,3.2-0.1,4.8c0,0.3,0,0.6,0,0.9c-1.4,0-2.7,0-4.1,0c0-3.9,0-7.9,0-11.9c1.3,0,2.5,0,3.9,0c0,0.8,0,1.6,0,2.4
-                            C99.5,31.1,101.1,30.2,103.6,30.5z"/>
-                        <path class="st0 st1" d="M217.8,29c0.7-1.3,1.3-2.6,2-3.8c0.1-0.1,0.2-0.2,0.3-0.2c1,0,1.9,0,3.1,0c-0.7,0.8-1.2,1.4-1.7,2
-                            c-0.3,0.4-0.8,0.8-1,1.2C219.8,29.1,218.9,29.2,217.8,29z"/>
-                    </g>
-                </svg>
-            </a>
-            <div class="collapse navbar-collapse " id="navbarExample">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <ins class="nav-link mos-table">Ligue Emergência 24h</ins>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('resultados')}}">Resultado de Exames</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('sobre')}}">Sobre </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('servicos')}}">Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('galeria')}}">Galeria</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('contato')}}">Contato</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+@extends('layouts.master')
+@section('content')
     <header class="masthead">
-        <div class="header-content">
-            <div class="header-content-inner">
-                <h1 id="homeHeading">Clínica Veterinária Arca de Noé</h1>
-                <hr>
-                <p>Clínica Veterinária Arca de Noé o melhor do atendimento <br>médico veterinário ao seu Pet de estimação</p>
-                <p>A Arca de Noé é uma clínica completa, uma referência em cuidados veterinários ao seu animal de estimação!</p>
-                <a class="btn btn-info btn-xl" href="{{route('sobre')}}">Explore nosso Site</a>
-            </div>
-        </div>
-        <div class="table-absolut">
-            <div class="table-fixed">
-                    <div class="col">
-                        <img src="/img/icon24.png"/>
-                        <h2>LIGUE EMERGÊNCIA 24H</h2>
-                    </div>
-                    <i class="fa fa-phone fa-5x"></i>
-                    <p>(11) 2561-2072</p>
-                    <p>(11) 98107-0544 (Nextel)</p>
-                    <p>(11) 98107-0647 (Nextel)</p>
-                    <p>Respondemos Whatsapp!</p>
-            </div>
-        </div>
-    </header>
-    <div class="slide-jssor">
+        <div class="slide-jssor">
         
         <!-- #region Jssor Slider Begin -->
     <!-- Generator: Jssor Slider Maker -->
@@ -314,6 +146,30 @@
         
         
     </div>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading">Clínica Veterinária Arca de Noé</h1>
+                    <hr>
+                <p>Clínica Veterinária Arca de Noé o melhor do atendimento <br>médico veterinário ao seu Pet de estimação</p>
+                <p>A Arca de Noé é uma clínica completa, uma referência em cuidados veterinários ao seu animal de estimação!</p>
+                <a class="btn btn-info btn-xl" href="{{route('sobre')}}">Explore nosso Site</a>
+            </div>
+        </div>
+        <div class="table-absolut">
+            <div class="table-fixed">
+                <div class="col">
+                    <img src="/img/icon24.png"/>
+                    <h2>LIGUE EMERGÊNCIA 24H</h2>
+                </div>
+                <i class="fa fa-phone fa-5x"></i>
+                <p>(11) 2561-2072</p>
+                <p>(11) 98107-0544 (Nextel)</p>
+                <p>(11) 98107-0647 (Nextel)</p>
+                <p>Respondemos Whatsapp!</p>
+            </div>
+        </div>
+    </header>
+    
     <section class="bg-info" id="sobre">
         <div class="container">
             <div class="row">
@@ -520,25 +376,4 @@
             </div>
         </div>
     </section>
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <span>&copy;&nbsp;Clínica Veterinária Arca de Noé Itaim paulista.Todos os direitos reservados.</span>  
-                    </div>
-                </div>
-            </div>
-        </footer>
-     <!-- Bootstrap core JavaScript -->
-    <script src="/js/componentes/jquery/jquery.min.js"></script>
-    <script src="/js/componentes/tether/tether.min.js"></script>
-    <script src="/js/componentes/bootstrap/js/bootstrap.min.js"></script>
-    <!-- Plugin JavaScript -->
-    <script src="/js/componentes/jquery-easing/jquery.easing.min.js"></script>
-    <script src="/js/componentes/scrollreveal/scrollreveal.min.js"></script>
-    <script src="/js/componentes/magnific-popup/jquery.magnific-popup.min.js"></script>
-    <!-- Custom scripts for this template -->
-    <script src="/js/creative.js"></script>
-</body>
-
-</html>
+@endsection
