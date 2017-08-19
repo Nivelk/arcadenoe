9f7f494d11a9f27ec993be5ced05681fa5422616
42143 <aslan-k@hotmail.com>
2017-08-19 03:19:05 -0300

    atualização do site clinica veterinaria arca de noe itaim paulista

---
+++ resources/views/layouts/gallery.blade.php
@@ -0,0 +1,142 @@
+<section class="no-padding" id="galeria">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/1.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/1.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Conteudo relacionado a Cardiologia texto curto mas relevante.
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/2.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/2.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/3.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/3.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/4.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/4.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/5.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/5.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/6.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/6.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/7.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/7.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/8.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/8.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="{{url('/img/portfolio/fullsize/9.jpg')}}">
+                        <img class="img-fluid" src="{{url('/img/portfolio/thumbnails/9.jpg')}}" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    
+                                </div>
+                                <div class="project-name">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                {{-- --}}
+            </div>
+        </div>
+    </section>
